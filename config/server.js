37fd7091846b8e982fafa22d2de7e091bc55aa7b
Nichole John Martinez <xin_martinez@yahoo.com>
2016-04-21 11:42:19 +0800

    Create config for server

---
+++ config/server.js
@@ -0,0 +1,7 @@
+
+// Set local variables // can be access to views
+global.siteTitle = 'SpaceWay';
+global.siteDescription = 'SpaceWay for blah blah blah Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nam magnam natus tempora cumque, aliquam deleniti voluptatibus voluptas. Repellat vel, et itaque commodi iste ab, laudantium voluptas deserunt nobis. Rerum!';
+global.siteCopy = 'Copyright © '+siteTitle+' 2015';
+global.authorName = 'Nichole John Martinez';
+global.authorContact = 'xinmartinez@gmail.com';
