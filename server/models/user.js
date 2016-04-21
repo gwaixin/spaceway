06f02056f90d0ae3a8d682ce7491a0130747a85a
Nichole John Martinez <xin_martinez@yahoo.com>
2016-04-21 18:21:13 +0800

    Add validation on email

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,16 @@
 'use strict';
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     username: DataTypes.STRING,
     password: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {isEmail: true}
+    },
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
     birthdate: DataTypes.DATE,
@@ -15,7 +22,41 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         // associations can be defined here
       }
+    },
+    instanceMethods: {
+      comparePassword: function(user, password) {
+       return (decrypt(user.password) == password);
+      }
+    }
+  }, { 
+    validate: {
+      bothCoordsOrNone: function() {
+        if ((this.latitude === null) !== (this.longitude === null)) {
+          throw new Error('Require either both latitude and longitude or neither')
+        }
+      }
     }
   });
+
+  User.beforeCreate(function(user, options) {
+    user.password = encrypt(user.password);
+    user.status = true;
+    return user;
+  });
+
+  function encrypt(text) {
+    var cipher = crypto.createCipher(algorithm,password)
+    var crypted = cipher.update(text,'utf8','hex')
+    crypted += cipher.final('hex');
+    return crypted;
+  }
+
+  function decrypt(text) {
+    var decipher = crypto.createDecipher(algorithm,password)
+    var dec = decipher.update(text,'hex','utf8')
+    dec += decipher.final('utf8');
+    return dec;
+  }
+
   return User;
 };
