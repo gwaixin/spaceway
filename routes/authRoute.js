b86a9eb4aaf0cca68402cd168bf5f5bcf3825239
Nichole John Martinez <xin_martinez@yahoo.com>
2016-04-21 18:20:48 +0800

    Add crypto for login

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,11 +2,22 @@ var express = require('express');
 var authRoute = express.Router();
 var bodyParser = require('body-parser');
 var models = require('../server/models/');
+var Sequelize = require('sequelize');
 
 authRoute.use(bodyParser.urlencoded({ extended: true }));
 
 authRoute.get('/login', function(req, res) {
 	res.render('base', { page: '/auth/login', title: 'Login'});
+});
+
+authRoute.post('/login', function(req, res) {
+	models.User.findOne({where:{username: req.body.username}}).then(function(user) {
+		if (user.comparePassword(user, req.body.password)) {
+			res.send('saktopassword');
+		} else {
+			res.send('failangpassword');
+		}
+	});
 });
 
 authRoute.get('/logout', function(req, res) {
@@ -25,12 +36,9 @@ authRoute.post('/register', function(req, res) {
     first_name: req.body.firstname,
     status: true
 	}).then(function(user) {
-		if (user) {
-			res.redirect('/auth/register_complete');
-		} else {
-			res.send('');
-			// has error
-		}
+		res.redirect('/auth/register_complete');
+	}).catch(function(error) {
+		res.send(error);
 	});
 });
 
