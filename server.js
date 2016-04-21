e6a69d737e21b2ab4f21cd7c0b41d02ee07113ce
Nichole John Martinez <xin_martinez@yahoo.com>
2016-04-21 11:43:32 +0800

    Fix server, setups for view and statics

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+require('./config/server');
+
+// Set view engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
+
+// Set statics files or asset
+app.use(express.static('node_modules'));
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+	var response = 'Hello Spacewayer';
+	res.render('base', {title: 'Welcome to SpaceWay!!', page: 'landing/index'});
+});
+
+// Prepare for io
+var http = require('http').Server(app);
+
+http.listen(3030, function() {
+	console.log('Example app listening on port 3030!');
+});
