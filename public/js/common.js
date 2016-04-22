2c7160117b0d32e11aadf9b9302ca46d531230de
Nichole John Martinez <xin_martinez@yahoo.com>
2016-04-22 16:22:54 +0800

    Add common script

---
+++ public/js/common.js
@@ -0,0 +1,11 @@
+function inArray(needle, haystack) {
+  var length = haystack.length;
+  for(var i = 0; i < length; i++) {
+    if(typeof haystack[i] == 'object') {
+      if(arrayCompare(haystack[i], needle)) return true;
+    } else {
+      if(haystack[i] == needle) return true;
+    }
+  }
+  return false;
+}
